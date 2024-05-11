859f28e13e56df497d37d8b1c0a600fa4db25f3c
Antonio Alves <antonio.jpsalves@gmail.com>
2024-05-11 20:06:20 -0300

    :lipstick: feat: adicionando a font que sera utilizada no projeto

--- App.tsx
+++ App.tsx
@@ -1,15 +1,36 @@
+/* eslint-disable camelcase */
 import { StatusBar, View } from 'react-native'
-import { Home } from './src/app/home'
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
+
+import { Home } from '@/app/home'
+
+import { Loading } from '@/components/Loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  })
   return (
-    <View style={{ flex: 1 }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#0C0C0C',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      {!fontsLoaded ? <Loading /> : <Home />}
     </View>
   )
 }
